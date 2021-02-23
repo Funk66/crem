2fe4dd86d7779f352dbd59660151808c34f60444
Guillermo Guirao Aguilar <ggaguilar@gmail.com>
2021-02-23 18:38:33 +0100

    Setup Firebase environment

--- src/providers/firebase/firebase.ts
+++ src/providers/firebase/firebase.ts
@@ -1,23 +1,22 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/auth";
 
 export interface Firebase {
   auth: any;
 }
 
 const config = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  apiKey: "AIzaSyBIauoiTATE_tPOq04bmFTUlV-TbsjeGyM",
+  authDomain: "crem21.firebaseapp.com",
+  projectId: "crem21",
+  storageBucket: "crem21.appspot.com",
+  messagingSenderId: "860973427087",
+  appId: "1:860973427087:web:aa4a36c81e464576cd43fa",
+  measurementId: "G-3PJVJFJ7LY",
 };
 
 if (!firebase.apps.length) {
-    firebase.initializeApp(config);
+  firebase.initializeApp(config);
 }
 
 export const auth = firebase.auth();
